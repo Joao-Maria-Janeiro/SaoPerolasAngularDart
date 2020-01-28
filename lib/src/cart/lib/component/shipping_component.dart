5a765ee5e37645f8488357d3895dec8f014cb914
Joao-Maria-Janeiro <joaomariajaneiro1@gmail.com>
2020-01-28 15:30:08 +0000

    Working on shipping, trying to add some encryption

--- lib/src/cart/lib/component/shipping_component.dart
+++ lib/src/cart/lib/component/shipping_component.dart
@@ -4,6 +4,8 @@ import 'dart:html';
 import 'package:angular/angular.dart';
 import 'package:angular_forms/angular_forms.dart';
 import 'package:angular_router/angular_router.dart';
+import 'package:encrypt/encrypt.dart';
+
 
 import '../../../constants.dart';
 
@@ -18,6 +20,29 @@ class ShippingComponent {
   String full_name, address, city, localidade, zip, country, cell, email;
 
   void submitShippingDetails() {
+    final key = Key.fromUtf8('my 32 length key................');
+
+    final encrypter = Encrypter(AES(key));
+
+    // final encrypted = encrypter.encrypt(plainText, iv: iv);
+    // final decrypted = encrypter.decrypt(encrypted, iv: iv);
+
+
+    window.localStorage['sao_perolas_shipping'] = jsonEncode(  
+      {
+        'full_name': encrypter.encrypt(full_name).base64,
+        'address': encrypter.encrypt(address).base64,
+        'city': city,
+        'localidade': localidade,
+        'zip': zip,
+        'country': country,
+        'cell': encrypter.encrypt(cell).base64,
+        'email': email
+      }
+    );
+    String test = jsonDecode(window.localStorage['sao_perolas_shipping'])['address'];
+    print(encrypter.decrypt64(test));
+    // print(encrypter.decrypt(encrypted, iv: iv));
     
   }
 }
