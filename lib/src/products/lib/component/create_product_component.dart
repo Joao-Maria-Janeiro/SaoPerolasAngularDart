e205e32f31e5abd19642f55dbf8452e6694b75e7
João Maria Janeiro <joaomariajaneiro1@gmail.com>
2020-02-29 00:51:18 +0000

    Working on the product creation page

--- lib/src/products/lib/component/create_product_component.dart
+++ lib/src/products/lib/component/create_product_component.dart
@@ -1,3 +1,7 @@
+@JS()
+library k;
+
+import 'dart:async';
 import 'dart:convert';
 import 'dart:html';
 
@@ -6,19 +10,29 @@ import 'package:angular_forms/angular_forms.dart';
 import 'package:angular_router/angular_router.dart';
 import 'package:saoperolas/src/products/lib/service/product_service.dart';
 import 'package:image/image.dart';
+import 'package:js/js.dart';
 
+@JS()
+external void letsCrop();
 
+class LetsCrop {
+  LetsCrop();
+  void get cropperWrapper => letsCrop();
+}
 
 @Component (
   selector: 'create-products',
   directives: [coreDirectives,formDirectives, routerDirectives],
   templateUrl: 'create_product_component.html',
+  styleUrls: ['create_product_component.css'],
 )
-class CreateProductComponent {
+class CreateProductComponent implements OnInit, AfterViewChecked {
   ProductService _service;
-  CreateProductComponent(this._service);
+  ProductService _productService;
+  CreateProductComponent(this._service, this._productService);
   File file = null;
   var trimmed = null;
+  List<dynamic> types;
 
   Future<void> onFileChanged(event) async {
     file = event.target.files[0];
@@ -28,6 +42,18 @@ class CreateProductComponent {
     Image image = decodeJpg(result);
 //    trimmed = encodePng(copyCrop(image, (image.width - 100)~/2, (image.height - 100)~/2, 100, 100));
 //    await _service.createProduct(base64Encode(result));
+  }
+
+  @override
+  void ngAfterViewChecked() {
+    Timer(Duration(seconds: 2), () {
+      LetsCrop().cropperWrapper;
+    });
+  }
+
+  @override
+  void ngOnInit() async {
+    types = await _service.getProductTypes();
   }
 
 }
